ca527aa32c5bd40b6fd1a669ead72235b47881aa
Joaquin H <hernandez.joaquin346@gmail.com>
2025-10-09 01:44:06 -0300

    signature

--- lib/ledger.ex
+++ lib/ledger.ex
@@ -5,82 +5,33 @@ defmodule Ledger.CLI do
   Module to handle command line interface for Ledger application.
   """
   def main(args) do
-    {status, config} = parse_args(args)
-    case {status, config} do
-      {:ok, arguments} ->
-        case arguments.subcommand do
-          "balance" ->
-            Commands.Balance.run(arguments)
-          "transacciones" ->
-            Commands.Transacciones.run(arguments)
-          _ -> IO.puts("Comando no reconocido")
-        end
-      {:error, reason} ->
-        IO.puts("#{reason}")
-    end
-  end
-
-  defp parse_args(args) do
     [command | arguments] = args
-    processed_args = preprocess_account_flags(arguments)
-    {options, remaining_args, errors} =
-      processed_args
-      |> OptionParser.parse(
-        aliases: [
-          t: :path_transacciones_data,
-          o: :output_path,
-          m: :moneda
-        ],
-        strict: [
-          subcommand: :string,
-          cuenta_origen: :string,
-          path_transacciones_data: :string,
-          cuenta_destino: :string,
-          output_path: :string,
-          moneda: :string
-        ]
-      )
-
-    case {options, remaining_args, errors} do
-      {opts, [], []} ->
-        opts =
-          default_args()
-          |> Map.merge(Map.new(opts))
-          |> Map.put(:subcommand, command)
-        {:ok, opts}
-
-      {_opts, remaining, []} ->
-        remaining
-        |> Enum.join(", ")
-        |> then(fn msg -> {:error, "#{msg} no se reconoce como opcion valida"} end)
-
-      {_opts, _remaining, errors} ->
-        errors
-        |> Enum.map(fn {key, val} -> "#{key}#{val}" end)
-        |> Enum.join(", ")
-        |> then(fn msg -> {:error, "#{msg} no se reconoce como opcion valida"} end)
+    arguments = parse_args(arguments)
+    case command do
+      "balance" ->
+        Commands.Balance.run(arguments)
+      "transacciones" ->
+        Commands.Transacciones.run(arguments)
+      _ ->
+        command = String.split(command, "_")
+        case command do
+          [verbo, "moneda"] ->
+            Commands.Monedas.run(verbo, arguments)
+          [verbo, "usuario"] ->
+            Commands.Usuario.run(verbo, arguments)
+          _ ->
+            Commands.Transacciones.run(command, arguments)
+        end
     end
   end
-
-  # Helper function to convert -c1 and -c2 to full option names
-  defp preprocess_account_flags(args) do
+  # returns a map with the parsed arguments as key-value pairs.
+  def parse_args(args) do
     args
-    |> Enum.flat_map(fn
-      "-c1" -> ["--cuenta-origen"]
-      "-c2" -> ["--cuenta-destino"]
-      arg -> [arg]
+    |> Enum.reduce(%{}, fn arg, acc ->
+      case String.split(arg, "=") do
+        [key, value] -> Map.put(acc, key, value)
+        _ -> acc
+      end
     end)
-  end
-
-  defp default_args() do
-    %{
-      subcommand: "transacciones",
-      cuenta_origen: "all",
-      path_transacciones_data: "data/transacciones.csv",
-      path_currencies_data: "data/monedas.csv",
-      cuenta_destino: "all",
-      output_path: "console",
-      moneda: "all",
-    }
   end
 end
