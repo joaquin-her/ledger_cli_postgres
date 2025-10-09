1938058efe666269ceac2b4f548d04c06662eb0f
Joaquin H <hernandez.joaquin346@gmail.com>
2025-10-09 02:43:14 -0300

    insert & see

--- lib/ledger/commands/monedas.ex
+++ lib/ledger/commands/monedas.ex
@@ -1,10 +1,40 @@
 defmodule Ledger.Commands.Monedas do
+  alias Ledger.Repo
+  alias Ledger.Schemas.Moneda
 
-  def run(_, args) do
-    IO.puts("Running monedas with args: \n#{inspect(args)}")
+  # crea una moneda
+  def run(:crear, args) do
+    moneda = %{
+      nombre: args["-n"],
+      precio_en_usd: String.to_float(args["-p"])
+    }
+    changeset = Moneda.changeset(%Moneda{}, moneda)
+    changeset
+    |> Ledger.Repo.insert()
   end
 
-  # defp create_moneda(attrs) do
-  #   Ledger.Schemas.Moneda.changeset(%Ledger.Schemas.Moneda{}, attrs[])
-  # end
+  # edita una moneda
+  def run(:editar, _) do
+
+  end
+
+  # borra una moneda
+  def run(:borrar, _) do
+
+  end
+
+  # lista una moneda
+  def run(:ver, args) do
+    case args["-id"] do
+      "all" ->
+        Repo.all(Moneda)
+      id ->
+        Repo.get!(Moneda, String.to_integer(id))
+    end
+  end
+
+  def run(operation, args) do
+    IO.puts("Running monedas with operation: #{operation} args: \n#{inspect(args)}")
+  end
+
 end
