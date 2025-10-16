28ab8a88ef7e7f09be37f8f6d5f31d75c97026ed
Joaquin H <hernandez.joaquin346@gmail.com>
2025-10-16 01:13:45 -0300

    extracted method

---
+++ lib/ledger/commands/utils.ex
@@ -0,0 +1,13 @@
+defmodule Ledger.Commands.Utils do
+
+  # Helper para formatear errores
+  def format_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+    |> Enum.map(fn {field, errors} -> "#{field}: #{Enum.join(errors, ", ")}" end)
+    |> Enum.join("; ")
+  end
+end
