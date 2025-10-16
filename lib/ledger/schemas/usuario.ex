13e15545ede03379b0475a6789368eddee0142d2
Joaquin H <hernandez.joaquin346@gmail.com>
2025-10-16 01:12:43 -0300

    extra_validation

--- lib/ledger/schemas/usuario.ex
+++ lib/ledger/schemas/usuario.ex
@@ -15,6 +15,7 @@ defmodule Ledger.Schemas.Usuario do
     usuario
     |> Changeset.cast(attrs, [:nombre_usuario, :fecha_nacimiento])
     |> Changeset.validate_required([:nombre_usuario, :fecha_nacimiento])
+    |> no_mantiene_nombre_usuario()
     |> validar_es_mayor_de_edad()
     |> Changeset.unique_constraint(:nombre_usuario)
     |> Changeset.validate_length(:nombre_usuario, min: 1)
@@ -25,6 +26,16 @@ defmodule Ledger.Schemas.Usuario do
     edad_minima = 18*365
     if fecha_nacimiento && Date.diff(Date.utc_today(), fecha_nacimiento) < edad_minima do
       Changeset.add_error(changeset, :fecha_nacimiento, "Debe ser mayor de edad")
+    else
+      changeset
+    end
+  end
+
+  defp no_mantiene_nombre_usuario(changeset) do
+    nombre_actual = changeset.data.nombre_usuario
+    nombre_nuevo = Changeset.get_field(changeset, :nombre_usuario)
+    if nombre_actual && nombre_actual == nombre_nuevo do
+      Changeset.add_error(changeset, :nombre_usuario, "El nombre de usuario debe ser diferente al actual")
     else
       changeset
     end
