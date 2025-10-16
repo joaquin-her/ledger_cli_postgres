5266067ced0ee47cb98ed71ec0d89c8536e9c00b
Joaquin H <hernandez.joaquin346@gmail.com>
2025-10-16 01:14:05 -0300

    muted warnings

--- test/commands/transacciones_command_test.exs
+++ test/commands/transacciones_command_test.exs
@@ -1,11 +1,12 @@
 defmodule Commands.TransaccionesCommandTest do
   use ExUnit.Case, async: true
-  alias Ledger.Schemas.Transaccion
-  alias Ledger.Commands.Transacciones
+  #alias Ledger.Schemas.Transaccion
+  #alias Ledger.Commands.Transacciones
 
   # Create transacciones
   test "transaccion de tipo alta cuenta = :ok" do
-
+    #args = %{""}
+    #{state, transaccion} = Transacciones.run(:crear, )
   end
   # Get transacciones
 
